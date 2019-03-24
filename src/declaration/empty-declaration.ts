3898d1c2fb9182f3a28e403ce0a772b711cf4492
Ruslan Lopatin <ruslan.lopatin@gmail.com>
2019-03-24 11:18:27 +0700

    Move empty declaration to dedicated internal module

---
+++ src/declaration/empty-declaration.ts
@@ -0,0 +1,35 @@
+import { StypDeclaration } from './declaration';
+import { stypSelector, StypSelector } from '../selector';
+import { noStypProperties } from './properties.impl';
+
+/**
+ * @internal
+ */
+export class EmptyStypDeclaration extends StypDeclaration {
+
+  get spec() {
+    return emptySpec;
+  }
+
+  constructor(
+      readonly root: StypDeclaration,
+      readonly selector: StypSelector.Normalized) {
+    super();
+  }
+
+  select(selector: StypSelector): StypDeclaration {
+
+    const _selector = stypSelector(selector);
+
+    if (!_selector.length) {
+      return this;
+    }
+
+    return new EmptyStypDeclaration(this.root, [...this.selector, ..._selector]);
+  }
+
+}
+
+function emptySpec() {
+  return noStypProperties;
+}
--- src/declaration/root.ts
+++ src/declaration/root.ts
@@ -1,43 +1,18 @@
 import { stypSelector, StypSelector } from '../selector';
 import { StypProperties } from './properties';
-import { noStypProperties, stypPropertiesBySpec } from './properties.impl';
+import { stypPropertiesBySpec } from './properties.impl';
 import { StypDeclaration } from './declaration';
+import { EmptyStypDeclaration } from './empty-declaration';
 
 const rootSelector: StypSelector.Normalized = [];
 let rootDeclaration: StypDeclaration | undefined;
 
-class EmptyDeclaration extends StypDeclaration {
-
-  get spec() {
-    return emptySpec;
-  }
-
-  constructor(
-      readonly root: StypDeclaration,
-      readonly selector: StypSelector.Normalized) {
-    super();
-  }
-
-  select(selector: StypSelector): StypDeclaration {
-
-    const _selector = stypSelector(selector);
-
-    if (!_selector.length) {
-      return this;
-    }
-
-    return new EmptyDeclaration(this.root, [...this.selector, ..._selector]);
-  }
-
-}
-
 export function stypRoot(properties?: StypProperties.Spec): StypDeclaration {
-
   if (!properties && rootDeclaration) {
     return rootDeclaration;
   }
 
-  class Root extends StypDeclaration {
+  class StypRoot extends StypDeclaration {
 
     readonly spec = (decl: StypDeclaration) => stypPropertiesBySpec(decl, properties);
 
@@ -57,20 +32,16 @@ export function stypRoot(properties?: StypProperties.Spec): StypDeclaration {
         return this;
       }
 
-      return new EmptyDeclaration(this, _selector);
+      return new EmptyStypDeclaration(this, _selector);
     }
 
   }
 
-  const root = new Root();
+  const root = new StypRoot();
 
   if (!properties && !rootDeclaration) {
     rootDeclaration = root;
   }
 
   return root;
-}
-
-function emptySpec() {
-  return noStypProperties;
 }
