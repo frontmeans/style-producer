ca8f574898bcb7c5b4ecbbe477b55f19346dcd4c
Ruslan Lopatin <ruslan.lopatin@gmail.com>
2020-11-27 10:54:09 +0700

    Use `@run-z/rollup-helpers`

--- rollup.config.js
+++ rollup.config.js
@@ -1,7 +1,7 @@
-import { externalModules } from '@proc7ts/rollup-helpers';
 import flatDts from '@proc7ts/rollup-plugin-flat-dts';
 import commonjs from '@rollup/plugin-commonjs';
 import nodeResolve from '@rollup/plugin-node-resolve';
+import { externalModules } from '@run-z/rollup-helpers';
 import sourcemaps from 'rollup-plugin-sourcemaps';
 import ts from 'rollup-plugin-typescript2';
 import typescript from 'typescript';
